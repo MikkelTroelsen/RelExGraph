ec8850cc5613bd76ca1f7c8e5aff8c6d12edd84f
Mikkel Søren Troelsen <Mikkel.s.troelsen@gmail.com>
2025-11-06 17:18:33 +0100

    relik example test

--- RelikService/main.py
+++ RelikService/main.py
@@ -1,1 +1,8 @@
+from relik import Relik
+from relik.inference.data.objects import RelikOutput
 
+relik = Relik.from_pretrained("sapienzanlp/relik-entity-linking-large")
+relik_out: RelikOutput = relik("Michael Jordan was one of the best players in the NBA.")
+
+print(relik_out.triplets)
+print(relik_out.candidates)
